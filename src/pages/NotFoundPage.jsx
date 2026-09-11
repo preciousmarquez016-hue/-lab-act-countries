e7ebbdf310bbf5f83ed8b5b9e9e2a4c609e444bf
Precious Marquez <preciousmarquez016@gmail.com>
2026-09-11 12:32:56 +0800

    Task 1: install react-router

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -28,18 +28,18 @@ const NotFoundPage = () => {
         </p>
 
         <div class="mt-10 flex items-center justify-center gap-x-6">
-          <a
-            href="#"
-            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          <Link
+            to="/"
+            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go back home
-          </a>
-          <a
-            href="#"
+          </Link>
+          <Link
+            to="/"
             class="text-sm font-semibold text-gray-900 hover:text-indigo-600"
           >
             Contact support <span aria-hidden="true">&rarr;</span>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
